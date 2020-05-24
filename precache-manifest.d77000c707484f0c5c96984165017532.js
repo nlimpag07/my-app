968a801f23db424e112c6aebbe6d762b6f58baec
Your Name <nlimpag07@gmail.com>
2020-05-24 11:02:26 +0400

    Updates

---
+++ precache-manifest.d77000c707484f0c5c96984165017532.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e9530ff9c49f51fbfbdfc7f1d9e6812",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "1d65f6121e6f5a427b6e",
+    "url": "/my-app/static/css/main.acb2ade7.chunk.css"
+  },
+  {
+    "revision": "32540ac14b0f9e67caa5",
+    "url": "/my-app/static/js/2.ac17b5d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.ac17b5d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d65f6121e6f5a427b6e",
+    "url": "/my-app/static/js/main.27a30506.chunk.js"
+  },
+  {
+    "revision": "660024b2ea62aa0a0b7b",
+    "url": "/my-app/static/js/runtime-main.5119dbb0.js"
+  }
+]);
